0eb494af1e5d535b871baa17e5df560c238f33a9
Yessi Sitanggang <149045250+ecii123@users.noreply.github.com>
2025-02-27 14:29:36 +0700

    CatatanPerilaku_Orangtua

--- resources/views/components/sidebarOrangTua.blade.php
+++ resources/views/components/sidebarOrangTua.blade.php
@@ -17,7 +17,6 @@
           {{ session('student_data.nim') ?? 'NIM Tidak Ditemukan' }}
         </p>
       </a>
-
     </div>
   </div>
   <ul class="menu">
@@ -34,54 +33,49 @@
     <li class="menu-item">
       <a href="javascript:void(0);" onclick="toggleSubMenu('perkuliahan-submenu')">
         <i class="fas fa-book"></i> Perkuliahan
-        <i class="fas fa-chevron-down submenu-toggle" id="perkuliahan-toggle"></i>
+        <i class="fas fa-chevron-left submenu-toggle" id="perkuliahan-toggle"></i>
       </a>
+      <ul class="submenu" id="perkuliahan-submenu" style="display: none;">
+        <li class="submenu-item">
+          <a href="{{ route('prodi') }}">
+            <i class="fas fa-user"></i> Prodi
+          </a>
+        </li>
+        <li class="submenu-item">
+          <a href="{{ route('jadwal') }}">
+            <i class="fas fa-calendar-alt"></i> Jadwal
+          </a>
+        </li>
+        <li class="submenu-item">
+          <a href="{{ route('absensi') }}">
+            <i class="fas fa-address-card"></i> Absensi Mahasiswa
+          </a>
+        </li>
+        <li class="submenu-item">
+          <a href="{{ route('kemajuan_studi') }}">
+            <i class="fas fa-chart-line"></i> Kemajuan Studi
+          </a>
+        </li>
+      </ul>
     </li>
-    <ul class="submenu" id="perkuliahan-submenu" style="display: none;">
-      <li class="submenu-item">
-        <a href="{{ route('prodi') }}">
-          <i class="fas fa-user"></i> Prodi
-        </a>
-      </li>
-      <li class="submenu-item">
-        <a href="{{ route('jadwal') }}">
-          <i class="fas fa-calendar-alt"></i> Jadwal
-        </a>
-      </li>
-      <li class="submenu-item">
-        <a href="{{ route('absensi') }}">
-          <i class="fas fa-address-card"></i> Absensi Mahasiswa
-        </a>
-      </li>
-      <li class="submenu-item">
-        <a href="{{ route('kemajuan_studi') }}">
-          <i class="fas fa-chart-line"></i> Kemajuan Studi
-        </a>
-      </li>
-    </ul>
-    </li>
-
-    <!-- Perizinan dengan submenu -->
     <li class="menu-item">
       <a href="javascript:void(0);" onclick="toggleSubMenu('perizinan-submenu')">
         <i class="fas fa-file-alt"></i> Perizinan
-        <i class="fas fa-chevron-down submenu-toggle" id="perizinan-toggle"></i>
+        <i class="fas fa-chevron-left submenu-toggle" id="perizinan-toggle"></i>
       </a>
+      <ul class="submenu" id="perizinan-submenu" style="display: none;">
+        <li class="submenu-item">
+          <a href="{{ route('izin_bermalam') }}">
+            <i class="fas fa-moon"></i> Izin Bermalam
+          </a>
+        </li>
+        <li class="submenu-item">
+          <a href="{{ route('izin_keluar') }}">
+            <i class="fas fa-walking"></i> Izin Keluar
+          </a>
+        </li>
+      </ul>
     </li>
-    <ul class="submenu" id="perizinan-submenu" style="display: none;">
-      <li class="submenu-item">
-        <a href="{{ route('izin_bermalam') }}">
-          <i class="fas fa-moon"></i> Izin Bermalam
-        </a>
-      </li>
-      <li class="submenu-item">
-        <a href="{{ route('izin_keluar') }}">
-          <i class="fas fa-walking"></i> Izin Keluar
-        </a>
-      </li>
-    </ul>
-    </li>
-
     <li class="menu-item">
       <a href="{{ route('asrama') }}">
         <i class="fas fa-building"></i> Asrama
