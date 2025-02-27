1007f0c3e211b3884b3f52d79b43222b946079ab
Yessi Sitanggang <149045250+ecii123@users.noreply.github.com>
2025-02-27 14:47:32 +0700

    cat perilaku

--- resources/views/components/sidebarMahasiswa.blade.php
+++ resources/views/components/sidebarMahasiswa.blade.php
@@ -1,40 +1,45 @@
 <div class="sidebar">
-    <div class="sidebar-header">
-        <img src="{{ asset('assets\img\Logo Institut Teknologi Del.png') }}" alt="Logo" class="header-logo">
-        <div class="header-text">
-            <h4>SiMBA</h4>
-            <p>Mahasiswa</p>
-        </div>
+  <div class="sidebar-header">
+    <img src="{{ asset('assets\img\Logo Institut Teknologi Del.png') }}" alt="Logo" class="header-logo">
+    <div class="header-text">
+      <h4>SiMBA</h4>
+      <p>Mahasiswa</p>
     </div>
-    <div class="profile-section">
-        <div class="profile-card">
-            <a href="{{ route('profil') }}">
-                <img src="{{ asset('assets/img/profil.jpg') }}" alt="Profile Picture" class="profile-picture">
-                <h4 class="profile-name">
-                    {{ session('student_data.nama') ?? 'Nama Tidak Ditemukan' }}
-                </h4>
-                <p class="profile-id">
-                    {{ session('student_data.nim') ?? 'NIM Tidak Ditemukan' }}
-                </p>
-            </a>
+  </div>
+  <div class="profile-section">
+    <div class="profile-card">
+      <a href="{{ route('profil') }}">
+        <img src="{{ asset('assets/img/profil.jpg') }}" alt="Profile Picture" class="profile-picture">
+        <h4 class="profile-name">
+          {{ session('student_data.nama') ?? 'Nama Tidak Ditemukan' }}
+        </h4>
+        <p class="profile-id">
+          {{ session('student_data.nim') ?? 'NIM Tidak Ditemukan' }}
+        </p>
+      </a>
 
-        </div>
     </div>
-    <ul class="menu">
-        <li class="menu-item">
-            <a href="{{ route('beranda') }}">
-                <i class="fas fa-home"></i> Beranda
-            </a>
-        </li>
-        <li class="menu-item">
-            <a href="{{ route('mahasiswa_konseling') }}">
-                <i class="fas fa-user-friends"></i> Konseling
-            </a>
-        </li>
-        <li class="menu-item">
-            <a href="{{ route('mahasiswa_perwalian') }}">
-                <i class="fas fa-list-alt"></i> Perwalian
-            </a>
-        </li>
-    </ul>
+  </div>
+  <ul class="menu">
+    <li class="menu-item">
+      <a href="{{ route('beranda') }}">
+        <i class="fas fa-home"></i> Beranda
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="{{ route('mahasiswa_konseling') }}">
+        <i class="fas fa-user-friends"></i> Konseling
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="{{ route('mahasiswa_perwalian') }}">
+        <i class="fas fa-list-alt"></i> Perwalian
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="{{ route('catatan_perilaku_orang_tua') }}">
+        <i class="fas fa-user-edit"></i> Catatan Perilaku
+      </a>
+    </li>
+  </ul>
 </div>
