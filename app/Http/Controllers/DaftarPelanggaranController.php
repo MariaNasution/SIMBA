ccfd7ec716de736584bbd30221c88dd3c5b3a45e
riovan716 <riovansihombing3@gmail.com>
2025-03-03 21:07:24 +0700

    pelanggaran mahasiswa use api

--- app/Http/Controllers/DaftarPelanggaranController.php
+++ app/Http/Controllers/DaftarPelanggaranController.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\DB;
 
 class DaftarPelanggaranController extends Controller
 {
@@ -11,42 +10,67 @@ class DaftarPelanggaranController extends Controller
     {
         $apiToken = session('api_token');
 
-        // Ambil daftar NIM beserta nama mahasiswa dari database
-        $nimData = DB::table('nim')->select('nim', 'nama')->get();
-        $pelanggaranList = collect();
+        try {
+            // Ambil data mahasiswa dari API pertama
+            $response = Http::withToken($apiToken)
+                ->withOptions(['verify' => false])
+                ->get("https://cis-dev.del.ac.id/api/library-api/mahasiswa");
 
-        foreach ($nimData as $data) {
-            try {
-                // Ambil data pelanggaran berdasarkan NIM
-                $pelanggaranResponse = Http::withToken($apiToken)
-                    ->withOptions(['verify' => false])
-                    ->get("https://cis-dev.del.ac.id/api/aktivitas-mhs-api/get-pelanggaran-mhs", [
-                        'nim' => $data->nim,
-                        'ta' => '',
-                        'sem_ta' => '',
-                    ]);
+            // Pastikan response sukses
+            if (!$response->successful() || !isset($response['data']['mahasiswa'])) {
+                Log::error('Gagal mengambil data mahasiswa.');
+                return view('konseling.daftar_pelanggaran', ['pelanggaranList' => collect()]);
+            }
 
-                if ($pelanggaranResponse->successful() && isset($pelanggaranResponse['data'])) {
+            // Ambil hanya NIM dan Nama mahasiswa
+            $mahasiswaData = collect($response['data']['mahasiswa'])->take(4)->map(function ($item) {
+                return [
+                    'nim' => $item['nim'] ?? '-',
+                    'nama' => $item['nama'] ?? '-',
+                ];
+            });
+
+            $pelanggaranList = collect();
+
+            // Looping setiap mahasiswa untuk mengambil data pelanggaran berdasarkan NIM
+            foreach ($mahasiswaData as $data) {
+                try {
+                    $pelanggaranResponse = Http::withToken($apiToken)
+                        ->withOptions(['verify' => false])
+                        ->get("https://cis-dev.del.ac.id/api/aktivitas-mhs-api/get-pelanggaran-mhs", [
+                            'nim' => $data['nim'],
+                            'ta' => '',
+                            'sem_ta' => '',
+                        ]);
+
+                    // Debugging: Cek apakah respons API kedua berhasil
+                    if (!$pelanggaranResponse->successful() || !isset($pelanggaranResponse['data'])) {
+                        Log::error("Gagal mengambil data pelanggaran untuk NIM: {$data['nim']}");
+                        continue;
+                    }
+
+                    // Mapping data pelanggaran
                     $pelanggaranItems = collect($pelanggaranResponse['data'])->map(function ($item) use ($data) {
                         return [
-                            'nim' => $data->nim,
-                            'nama' => $data->nama, // Nama langsung dari database
+                            'nim' => $data['nim'],
+                            'nama' => $data['nama'], // Ambil nama dari API pertama
                             'pelanggaran' => $item['pelanggaran'] ?? '-',
                             'tingkat' => $item['tingkat'] ?? '-'
                         ];
                     });
 
+                    // Gabungkan ke dalam list pelanggaran
                     $pelanggaranList = $pelanggaranList->merge($pelanggaranItems);
+                } catch (\Exception $e) {
+                    Log::error("Error mengambil data pelanggaran untuk NIM: {$data['nim']}", ['message' => $e->getMessage()]);
                 }
-            } catch (\Exception $e) {
-                Log::error('Exception terjadi:', ['message' => $e->getMessage()]);
             }
+
+        } catch (\Exception $e) {
+            Log::error('Exception terjadi saat mengambil data mahasiswa:', ['message' => $e->getMessage()]);
+            $pelanggaranList = collect(); // Kosongkan jika gagal mengambil data
         }
-
-
 
         return view('konseling.daftar_pelanggaran', ['pelanggaranList' => $pelanggaranList]);
     }
-
 }
-
