60c317eb4d42f829d68584409e81892448658974
adtyarhz <149128711+Adtyarhz@users.noreply.github.com>
2025-04-09 21:10:47 +0700

    Update mahasiswa_konseling.blade.php

--- resources/views/mahasiswa/mahasiswa_konseling.blade.php
+++ resources/views/mahasiswa/mahasiswa_konseling.blade.php
@@ -6,8 +6,6 @@
   use Carbon\Carbon;
 @endphp
 
-
-
 <!-- Tombol Request Konseling -->
 <div class="mb-3 text-start">
   <a href="{{ route('mhs_konseling_request') }}"
@@ -121,6 +119,4 @@
     }
   });
 </script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 @endsection
